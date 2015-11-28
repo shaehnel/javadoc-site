4ef1ba3dc516be81149c5d4e66f7927153282047
Sebastian Haehnel <sebastian.haehnel@gmail.com>
2015-11-28 17:55:08 +0100

    adding mvn site and release

--- javadoc-site-module-1/src/main/java/com/foo/Service.java
+++ javadoc-site-module-1/src/main/java/com/foo/Service.java
@@ -1,7 +1,15 @@
 package com.foo;
 
+/**
+ * the service
+ */
 public class Service {
 
+    /**
+     * creates a greeting
+     * @param s thing to greet
+     * @return the greeting
+     */
     public String hello(String s) {
         return "Hello "+s;
     }
--- javadoc-site-module-2/src/main/java/com/foo/Program.java
+++ javadoc-site-module-2/src/main/java/com/foo/Program.java
@@ -1,9 +1,15 @@
 package com.foo;
 
-
+/**
+ * the program
+ */
 public class Program {
     Service service = new Service();
 
+    /**
+     * the greeter method
+     * @param name
+     */
     public void greeter(String name) {
         System.out.println(service.hello(name));
     }
